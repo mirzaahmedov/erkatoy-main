3b7c076d04dde9396a84af4ff5219b3ca06c6c0b
Mirzaahmedov Bekzod <mirzaahmedov.dev@gmail.com>
2025-12-15 14:25:39 +0500

    update baseurl

--- src/common/lib/http.ts
+++ src/common/lib/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "@/stores/auth";
 
-export const baseURL = "http://192.168.102.68:4001";
+export const baseURL = "http://217.18.63.71:4009";
 
 export const client = axios.create({
   baseURL,
